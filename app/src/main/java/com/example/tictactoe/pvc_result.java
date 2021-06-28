8722d27dccc8599855b72ed4de783022966274ba
LAPTOP-F180Q5N9\a0976 <a0976459002@gmail.com>
2021-06-29 02:01:18 +0800

    Revert "no message"
    This reverts commit c8dd74a5dcfff860a3b8a1c102f944c60e56932a.

--- app/src/main/java/com/example/tictactoe/pvc_result.java
+++
@@ -1,7 +0,0 @@
-package com.example.tictactoe;
-
-public class pvc_result {
-    public static int drw_rlt = 0;
-    public static int com_rlt = 0;
-    public static int ply_rlt = 0;
-}
--- app/src/main/java/com/example/tictactoe/pvp_result.java
+++
@@ -1,7 +0,0 @@
-package com.example.tictactoe;
-
-public class pvp_result {
-    public static int drw_rlt = 0;
-    public static int com1_rlt = 0;
-    public static int com2_rlt = 0;
-}
